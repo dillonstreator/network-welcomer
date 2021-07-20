5b007bdcb5c3eca6516ee6bcb604a37e1e58cf9f
dillon <dillonstreator@gmail.com>
2021-07-19 22:18:15 -0500

    refactor

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 const say = require("say");
 const { exec } = require("child_process");
+const ips = require("./ips.json");
 
-require("dotenv").config();
+const handlePinging = ({
+  ip,
+  name,
+  greeting = "Welcome to the network, {{NAME}}",
+  allowedConsecutiveTimeouts = 2
+}) => {
+  const pinger = exec(`ping -t ${ip}`);
 
-const { IP, PERSON, ALLOWED_CONSECUTIVE_TIMEOUTS = 2 } = process.env;
-if (!IP || !PERSON) {
-  console.error("`IP` && `PERSON` environment variables are required");
-  process.exit(1);
+  let isHere = false;
+  let numConsecutiveTimeouts = 0;
+  pinger.stdout.on("data", (d) => {
+    const message = d.toString().trim();
+    console.log(`${ip} ${message}`)
+
+    // misc output that we don't care about
+    if (["Request","Reply"].every(m => !message.includes(m))) return;
+
+    if (message.indexOf(ip) == -1) {
+      numConsecutiveTimeouts++;
+      if (numConsecutiveTimeouts > allowedConsecutiveTimeouts) isHere = false;
+      return;
+    }
+    numConsecutiveTimeouts = 0;
+    if (isHere) return;
+    isHere = true;
+
+    speakQueue.push(greeting.replace("{{NAME}}", name));
+  });
 }
 
-const pinger = exec(`ping -t ${IP}`);
+ips.forEach(handlePinging);
 
-let isHere = false;
-let numConsecutiveTimeouts = 0;
-pinger.stdout.on("data", (d) => {
-  const message = d.toString().trim();
-
-  // misc output that we don't care about
-  if (["Request","Reply"].every(m => !message.includes(m))) return;
-
-  if (message.indexOf(IP) == -1) {
-    numConsecutiveTimeouts++;
-    if (numConsecutiveTimeouts > ALLOWED_CONSECUTIVE_TIMEOUTS) isHere = false;
-    return;
-  }
-  numConsecutiveTimeouts = 0;
-  if (isHere) return;
-  isHere = true;
-
-  say.speak(`Welcome to the network, ${PERSON}`);
-});
