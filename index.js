73b2205b0ec716d5713230a569f0d1c5aeab6784
dillon <dillonstreator@gmail.com>
2021-09-01 16:43:48 -0500

    resilient pinging

--- index.js
+++ index.js
@@ -12,36 +12,48 @@ const {
 
 const greetingQueue = [];
 
-const handlePinging = ({
-  ip,
-  greeting,
-  allowedConsecutiveMissedPings = ALLOWED_CONSECUTIVE_MISSED_PINGS
-}) => {
-  const pinger = exec(`ping -t ${ip}`);
-
-  let isHere = false;
-  let numConsecutiveMissedPings = 0;
-  pinger.stdout.on("data", (d) => {
-    const message = d.toString().trim();
-    logger.trace(`[${ip}] ${message}`)
-
-    // misc output that we don't care about
-    if (["Request","Reply"].every(m => !message.includes(m))) return;
-
-    if (message.indexOf(ip) == -1) {
-      numConsecutiveMissedPings++;
-      if (isHere && numConsecutiveMissedPings > allowedConsecutiveMissedPings) {
-        logger.info(`[${ip}] has missed ${numConsecutiveMissedPings} consecutive pings`)
-        isHere = false
-      };
-      return;
-    }
-    numConsecutiveMissedPings = 0;
-    if (isHere) return;
-    isHere = true;
-
-    greetingQueue.push(greeting);
-  });
+const handlePinging = (params) => {
+  const doPing = ({
+    ip,
+    greeting,
+    allowedConsecutiveMissedPings = ALLOWED_CONSECUTIVE_MISSED_PINGS
+  }) => {
+    const pinger = exec(`ping -t ${ip}`);
+  
+    let isHere = false;
+    let numConsecutiveMissedPings = 0;
+    pinger.stdout.on("data", (d) => {
+      const message = d.toString().trim();
+      logger.trace(`[${ip}] ${message}`)
+  
+      // misc output that we don't care about
+      if (["Request","Reply"].every(m => !message.includes(m))) return;
+  
+      if (message.indexOf(ip) == -1) {
+        numConsecutiveMissedPings++;
+        if (isHere && numConsecutiveMissedPings > allowedConsecutiveMissedPings) {
+          logger.info(`[${ip}] has missed ${numConsecutiveMissedPings} consecutive pings`)
+          isHere = false
+        };
+        return;
+      }
+      numConsecutiveMissedPings = 0;
+      if (isHere) return;
+      isHere = true;
+  
+      greetingQueue.push(greeting);
+    });
+    const retry = () => doPing({ ip, greeting, allowedConsecutiveMissedPings })
+    pinger.on('close', (code) => {
+      logger.warn(`[${ip}] closed with code ${code}`)
+      retry()
+    })
+    pinger.on('error', (err) => {
+      logger.warn(`[${ip}] error with`, err)
+      retry()
+    })
+  }
+  doPing(params)
 }
 
 ips.forEach(handlePinging);
